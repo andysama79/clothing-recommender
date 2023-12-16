c216eb8cf1c3b9cd24809875af61c4fef0d6c041
andysama79 <arnab.dey@plaksha.edu.in>
2023-12-16 16:18:24 +0530

    final

--- VIBE/test.py
+++ VIBE/test.py
@@ -1,17 +1,7 @@
 import cv2
-from smplx import SMPLX
+import mediapipe as mp
+import matplotlib.pyplot as plt
 import numpy as np
 
-
-impath = 'dataset/humans/images/Helen.png'
-img = cv2.imread(impath)
-
-smpl = SMPLX(model_path='../smplx/models/SMPL_python_v.1.1.0/smpl/models/basicmodel_f_lbs_10_207_0_v1.1.0.pkl')
-
-result = smpl(img)
-
-print(result.keys())
-
-mesh = smpl.get_mesh(result)
-
-smpl.visualize(img, result, mesh)
+plt.plot([1,2,3,4], [1,4,9,16], 'ro')
+plt.show()
